4a2927e49df746176b3f5d4d3e0bdfaa3f321109
hyunbell <man.of.backend@gmail.com>
2023-08-01 21:09:30 +0900

    [TSK-34] chore: admin only test

--- apps/web/components/voca-form.tsx
+++ apps/web/components/voca-form.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useAuth, useOrganization } from '@clerk/nextjs';
 import { User } from '@clerk/nextjs/dist/types/server'
 import React, { useEffect, useState } from 'react'
 
@@ -6,8 +7,17 @@ interface VocaFormProps {
   name: string;
 }
 export default function VocaForm({ name }: VocaFormProps) {
+  const { orgId, orgRole, actor, isSignedIn } = useAuth()
+  const { organization, membership } = useOrganization()
 
+  console.log(orgId, orgRole, actor, organization, membership, isSignedIn)
   return (
-    <div>{name}</div>
+    <div className='flex flex-col gap-2'>
+      {name}
+      {orgId}
+      {orgRole}
+      {organization?.name}
+      {membership?.id}
+    </div>
   )
 }
