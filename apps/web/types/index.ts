cf5fde47331e99f40840f4db3467c831a6bf070b
hyunbell <man.of.backend@gmail.com>
2023-08-09 12:50:41 +0900

    Revert "fix: error message"
    This reverts commit 3a4f7c3cb4bcdc9d763f2fdb452d892768e253e0.

--- apps/web/types/index.ts
+++ apps/web/types/index.ts
@@ -10,8 +10,4 @@ export type Word = z.infer<typeof wordSchema>
 
 export type InputWord = z.infer<typeof wordInputSchema>
 
-export type CommonErrorCode = 'too_many_requests'
-
-export type SignInErrorCode = CommonErrorCode | 'form_password_incorrect' | 'form_identifier_not_found'
-
-export type SignUpErrorCode = CommonErrorCode | 'form_identifier_taken'
+export type SignInErrorCode = 'form_password_incorrect' | 'form_identifier_not_found'
