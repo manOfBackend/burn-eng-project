dcdb986065a3c2b9995b6b82c782b704b2675cf1
hyunbell <man.of.backend@gmail.com>
2023-11-04 15:25:39 +0900

    fix: 문제 난이도

--- apps/web/components/problem/writing-problem-view.tsx
+++ apps/web/components/problem/writing-problem-view.tsx
@@ -1,52 +1,61 @@
-'use client'
+"use client"
 
-import React, { useState } from 'react'
-import WritingProblemForm from './writing-problem-form'
-import { useMutation, useQuery } from '@tanstack/react-query'
-import { getSentenceProblem, getUserInfo, submitWriting } from '@sayvoca/lib/api'
-import { useRouter } from 'next/navigation'
-import { useFeedbackStore } from '@/store/feedback'
-import { InputSentence } from '@sayvoca/lib/types'
+import React, { useState } from "react"
+import WritingProblemForm from "./writing-problem-form"
+import { useMutation, useQuery } from "@tanstack/react-query"
+import {
+  getSentenceProblem,
+  getUserInfo,
+  submitWriting,
+} from "@sayvoca/lib/api"
+import { useRouter } from "next/navigation"
+import { useFeedbackStore } from "@/store/feedback"
+import { InputSentence } from "@sayvoca/lib/types"
 
 export default function WritingProblemView() {
-
   const router = useRouter()
-
 
   const { addFeedback } = useFeedbackStore()
 
   const { data: user } = useQuery({
-    queryKey: ['users'],
+    queryKey: ["users"],
     queryFn: () => getUserInfo(),
   })
 
-
   const { data: problem } = useQuery({
-    queryKey: ['sentence-random'],
+    queryKey: ["sentence-random"],
     queryFn: () => getSentenceProblem({ level: user?.level ?? 1 }),
     enabled: Boolean(user),
   })
 
-  const { mutate: submit, isLoading, isSuccess } = useMutation({
-    mutationKey: ['writing'],
+  const {
+    mutate: submit,
+    isLoading,
+    isSuccess,
+  } = useMutation({
+    mutationKey: ["writing"],
     mutationFn: submitWriting,
     onSuccess: (data) => {
       addFeedback(data)
-      router.push('/writing/result')
-    }
+      router.push("/writing/result")
+    },
   })
 
-
   function onSubmit(data: InputSentence) {
-    if (!problem) return;
+    if (!problem) return
     submit({
       sentenceId: problem.id,
-      translatedLanguage: 'ENGLISH',
+      translatedLanguage: "ENGLISH",
       translatedSentence: data.sentence,
     })
   }
 
   return (
-    <WritingProblemForm isLoading={isLoading || isSuccess} level={user?.level ?? 1} problem={problem?.sentence ?? ''} onSubmit={onSubmit} />
+    <WritingProblemForm
+      isLoading={isLoading || isSuccess}
+      level={problem?.level}
+      problem={problem?.sentence ?? ""}
+      onSubmit={onSubmit}
+    />
   )
 }
