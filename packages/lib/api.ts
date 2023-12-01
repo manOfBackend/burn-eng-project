cea8fa6de3e62f8767b51c3bbd21075155f13990
hyunbell <man.of.backend@gmail.com>
2023-12-02 02:55:38 +0900

    [BURNENG-14] fix: add http no auth

--- packages/lib/api.ts
+++ packages/lib/api.ts
@@ -1,46 +1,50 @@
 import { InputSentence, Sentence, SentenceHistoryDatesResponse, SentenceHistoryResponse, SentenceLevelResponse, SentencePage, SentenceProblemResponse, SentenceResponse, UserInfoResponse } from '../lib/types';
-import { http } from './http';
+import { http, httpWithAuth } from './http';
 
 export function getSentencePage({ page, size }: { page: number, size: number }) {
-  return http.get<SentencePage>('/admin/sentence', { params: { page, size } });
+  return httpWithAuth.get<SentencePage>('/admin/sentence', { params: { page, size } });
 }
 
 export function getSentenceLevelCount() {
-  return http.get<SentenceLevelResponse>('/admin/level/to/sentence-count');
+  return httpWithAuth.get<SentenceLevelResponse>('/admin/level/to/sentence-count');
 }
 
 export function addSentence({ language, sentence }: InputSentence) {
-  return http.post<Sentence>('/admin/sentence', { language, sentence });
+  return httpWithAuth.post<Sentence>('/admin/sentence', { language, sentence });
 }
 
 export function editSentence({ enable, id, language, level, sentence }: Pick<Sentence, 'id' | 'enable' | 'language' | 'sentence' | 'level'>) {
-  return http.put<void>(`/admin/sentence`, { enable, id, language, level, sentence });
+  return httpWithAuth.put<void>(`/admin/sentence`, { enable, id, language, level, sentence });
 }
 
 export function deleteSentence(sentenceId: Sentence['id']) {
-  return http.delete<void>(`/admin/sentence/${sentenceId}`);
+  return httpWithAuth.delete<void>(`/admin/sentence/${sentenceId}`);
 }
 
 export function submitWriting({ sentenceId, translatedLanguage, translatedSentence }: { translatedLanguage: string, translatedSentence: string, sentenceId: number }) {
+  return httpWithAuth.post<SentenceResponse>('/translated-feedback', { translatedLanguage, translatedSentence, sentenceId });
+}
+
+export function submitGuestWriting({ sentenceId, translatedLanguage, translatedSentence }: { translatedLanguage: string, translatedSentence: string, sentenceId: number }) {
   return http.post<SentenceResponse>('/translated-feedback', { translatedLanguage, translatedSentence, sentenceId });
 }
 
 export function searchSentence({ search }: { search: string }) {
-  return http.post<Sentence[]>('/admin/sentence/search', { search });
+  return httpWithAuth.post<Sentence[]>('/admin/sentence/search', { search });
 }
 
 export function getSentenceProblem({ level }: { level?: number } = {}) {
-  return http.get<SentenceProblemResponse>(`/sentence/random`, { params: { level } });
+  return httpWithAuth.get<SentenceProblemResponse>(`/sentence/random`, { params: { level } });
 }
 
 export function getHistoryDates({ languageFrom, languageTo, month, year }: { year: number, month: number, languageFrom: Sentence['language'], languageTo: Sentence['language'] }) {
-  return http.get<SentenceHistoryDatesResponse>(`/translated/history/dates`, { params: { languageFrom, languageTo, month, year } });
+  return httpWithAuth.get<SentenceHistoryDatesResponse>(`/translated/history/dates`, { params: { languageFrom, languageTo, month, year } });
 }
 
 export function getSentenceProblemHistory({ date, languageFrom, languageTo }: { date: string, languageFrom: string, languageTo: string }) {
-  return http.get<SentenceHistoryResponse>('/translated/history', { params: { languageFrom, languageTo, date } });
+  return httpWithAuth.get<SentenceHistoryResponse>('/translated/history', { params: { languageFrom, languageTo, date } });
 }
 
 export function getUserInfo() {
-  return http.get<UserInfoResponse>(`/users`);
+  return httpWithAuth.get<UserInfoResponse>(`/users`);
 }
--- packages/lib/http.ts
+++ packages/lib/http.ts
@@ -2,20 +2,22 @@ import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import Cookies from 'js-cookie';
 import { env } from './env.mjs'
 
-const axiosInstance = axios.create();
+const axiosInstanceWithAuth = axios.create();
+const axiosInstanceWithoutAuth = axios.create();
 
 export interface HttpClient extends AxiosInstance {
   get<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<T>;
   post<T = unknown>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
 }
 
-export const http: HttpClient = axiosInstance;
+export const httpWithAuth: HttpClient = axiosInstanceWithAuth;
+export const http: HttpClient = axiosInstanceWithoutAuth;
 
 const MOCK_API_URL = 'http://localhost:3000'
 
-http.interceptors.response.use(res => res.data);
+httpWithAuth.interceptors.response.use(res => res.data);
 
-http.interceptors.request.use((config) => {
+httpWithAuth.interceptors.request.use((config) => {
   if (!config.headers) return config;
   config.baseURL = env.NEXT_PUBLIC_IS_USE_MOCK ? MOCK_API_URL : env.NEXT_PUBLIC_API_URL;
   const sessionToken = Cookies.get('__session');
